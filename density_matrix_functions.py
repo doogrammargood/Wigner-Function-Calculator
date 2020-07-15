fixed covariance bug.

density_matrix_functions.py
@@ -2,7 +2,8 @@ import itertools
 import numpy as np
 import math
 import traceback
-from sl_class import *
+#from sl_class import *
+from covariance_functions import *
 from wigner_function import *
 def is_unitary(m):
     return np.allclose(np.eye(m.shape[0]), np.matmul(m.H, m) )
@@ -60,12 +61,18 @@ def cat_state(p,n):
     vector = [1.0 if all_equal(finite_field_element.from_int(i,p,n).coordinates) else 0 for i in range(p**n)]
     return matrix_from_list(vector)
 
-def state_with_special_order(p, i=4):#assume n = 1 for now..
-    s = sl_matrix.gen_with_order(p,1).__next__()
-    unitary = unitary_from_sl(s,p)
+def state_from_sl(sl, index, p, n):
+    unitary = unitary_from_sl(sl, p, n)
+    assert(is_unitary(unitary))
     eig = np.linalg.eig(unitary)
-    eig = eig[1][:,i].T
-    return matrix_from_list(eig)
+    assert np.allclose(unitary @ eig[1][:,index], eig[0][index] * eig[1][:,index])
+    eig = eig[1][:,index]
+    #print(eig)
+    eig = np.matrix(eig)
+    assert np.allclose((unitary @ eig).H, eig.H @ unitary.H)
+    assert np.allclose(eig @ eig.H, unitary @ eig @ eig.H @ unitary.H)
+    return np.matmul(eig,eig.H)
+    #return matrix_from_list(eig)
 
 def matrix_from_gross():
     p=3
@@ -87,28 +94,33 @@ def matrix_from_gross():
     dense_matrix = 1/3*(a + b + c)
     return dense_matrix
 
-def unitary_from_sl(mat, p):
-    #Assumes p is prime.
-    alpha = int(mat.matrix[0][0])
-    beta = int(mat.matrix[0][1])
-    gamma = int(mat.matrix[1][0])
-    epsilon = int(mat.matrix[1][1])
-    omega = np.exp((2*np.pi*1j)/float(p))
-    tau = omega ** modinv(2,p)
-    if p==2:
-        tau = 1j
-    array = []
-    if beta == 0:
-        array = [[tau**(alpha*gamma*r**2) if (alpha*r)%p==c else 0 for c in range(p)] for r in range(p)]
-        return np.matrix(array)
-    else:
-        for j in range(p):
-            row = []
-            for k in range(p):
-                #print (alpha*k**2 - 2*j*k + epsilon*j**2)%p *(beta**-1)
-                row.append(p**-0.5 * tau** ( (alpha*k**2 - 2*j*k + epsilon*j**2) *(beta**-1) ))
-            array.append(row)
-        return np.matrix(array)
+def unitary_from_sl(sl, p, n):
+    sp = sl.sl_matrix_to_prime_matrix(p,n)
+    unitary = sp.weil_representation()
+    return unitary
+
+# def unitary_from_sl(mat, p): This is deprecated. It only handled prime matrices.
+#     #Assumes p is prime.
+#     alpha = int(mat.matrix[0][0])
+#     beta = int(mat.matrix[0][1])
+#     gamma = int(mat.matrix[1][0])
+#     epsilon = int(mat.matrix[1][1])
+#     omega = np.exp((2*np.pi*1j)/float(p))
+#     tau = omega ** modinv(2,p)
+#     if p==2:
+#         tau = 1j
+#     array = []
+#     if beta == 0:
+#         array = [[tau**(alpha*gamma*r**2) if (alpha*r)%p==c else 0 for c in range(p)] for r in range(p)]
+#         return np.matrix(array)
+#     else:
+#         for j in range(p):
+#             row = []
+#             for k in range(p):
+#                 #print (alpha*k**2 - 2*j*k + epsilon*j**2)%p *(beta**-1)
+#                 row.append(p**-0.5 * tau** ( (alpha*k**2 - 2*j*k + epsilon*j**2) *(beta**-1) ))
+#             array.append(row)
+#         return np.matrix(array)
 
 def test_states():
     epsilon = 10**-7

finite_matrix_class.py
@@ -87,11 +87,11 @@ class finite_matrix(object):
             self.p = list_representation[0][0].p
             self.n = list_representation[0][0].n
 
-            for row in range(len(list_representation) - 1):
-                for col in range(len(list_representation[0]) - 1):
+            # for row in range(len(list_representation) - 1):
+            #     for col in range(len(list_representation[0]) - 1):
                     #check shape of blocks
-                    assert len(list_representation[row][col].elements[0]) == len(list_representation[row+1][col].elements[0])
-                    assert len(list_representation[row][col].elements) == len(list_representation[row][col+1].elements)
+                    #assert len(list_representation[row][col].elements[0]) == len(list_representation[row+1][col].elements[0])
+                    #assert len(list_representation[row][col].elements) == len(list_representation[row][col+1].elements)
             self.elements = []
             new_row = []
             for row in range(len(list_representation) ):
@@ -112,7 +112,7 @@ class finite_matrix(object):
 
     def __mul__(self, other):
         w =len(self.elements[0])
-        assert w == len(other.elements)
+        #assert w == len(other.elements)
         to_return = finite_matrix([[ reduce( (lambda x,y: x+y), [self.elements[c][k]*other.elements[k][r] for k in range(w)]) for c in range(len(self.elements))]for r in range(len(other.elements[0]))])
         return to_return.transpose()
 
@@ -123,7 +123,7 @@ class finite_matrix(object):
     def order(self):
         counter = 1
         curr = self.copy()
-        assert len(self.elements)==len(self.elements[0])
+        #assert len(self.elements)==len(self.elements[0])
         I = finite_matrix.identity(len(self.elements),self.p,self.n)
         while(not curr == I):
             curr = curr*self
@@ -157,7 +157,7 @@ class finite_matrix(object):
         return s
 
     def column_matrix_to_vector(self):
-        assert len(self.elements[0])==1
+        #assert len(self.elements[0])==1
         return self.transpose().elements[0]
 
     def is_symplectic(self):
@@ -180,11 +180,12 @@ class finite_matrix(object):
 
     def character(self):
         #from https://arxiv.org/pdf/1906.07230.pdf
-        assert len(self.elements)==len(self.elements[0])==1
+        #assert len(self.elements)==len(self.elements[0])==1
+        #Assumes the matrix is 1x1
         lamb = self.elements[0][0]
-        assert lamb.n == 1
+        #assert lamb.n == 1
         p = lamb.p
-        return np.e**(1j*2*np.pi*int(lamb)/p)
+        return np.e**(1j*2*np.pi*int(lamb.trace())/p)
     @classmethod
     def identity(cls,size,p,n):
         return finite_matrix( [[ finite_field_element.one(p,n) if c ==r else finite_field_element.zero(p,n) for c in range(size)]for r in range(size)] )
@@ -195,7 +196,7 @@ class finite_matrix(object):
 
     @classmethod
     def symplectic_form(cls,size,p,n):
-        assert size%2==0
+        #assert size%2==0
         I = finite_matrix.identity(size//2,p,n)
         O = finite_matrix.zero(size//2,p,n)
         to_return = finite_matrix([[O,I],[-I,O]])
@@ -268,6 +269,8 @@ class finite_matrix(object):
 
     @classmethod
     def convert_to_dual(cls, ffe):
+        # if ffe.n == 1:
+        #     return ffe
         col_mat = cls.dual_basis_conversion[(ffe.p,ffe.n)] * finite_matrix(ffe)
         return finite_field_element.from_vector(col_mat.column_matrix_to_vector())
 
@@ -279,7 +282,7 @@ class finite_matrix(object):
         pivot = None
         h = len(elts)
         w = len(elts[0])
-        assert w==h
+        #assert w==h
         size = w
         iden = finite_matrix.identity(size,self.p,self.n).elements
         x_index = 0 #No columns have been cleared
@@ -299,7 +302,7 @@ class finite_matrix(object):
 
             elts, iden = clear_column(elts, iden, top, x_index)
             top += 1
-        assert finite_matrix(elts) == finite_matrix.identity(size, self.p,self.n)
+        #assert finite_matrix(elts) == finite_matrix.identity(size, self.p,self.n)
         return finite_matrix(iden)
 
     def determinant(self):
@@ -310,7 +313,7 @@ class finite_matrix(object):
         #top = 0
         det = finite_field_element.one(self.p,self.n)
         #print(size)
-        assert len(elts[0])==size #determinant is only defined for square matrices
+        #assert len(elts[0])==size #determinant is only defined for square matrices
         for x_index in range(size):
             pivot = find_nonzero(elts,x_index,size,x_index)
             if pivot is None:
@@ -330,7 +333,7 @@ class finite_matrix(object):
         elts = self.elements.copy()
         pivot = None
         size = len(elts)
-        assert len(elts[0])>=size
+        #assert len(elts[0])>=size
         current_row = 0
         permute = [i for i in range(len(elts))] #initially, the identity.
         for x_index in range(len(elts[0])):

finite_sp_matrix_class.py
@@ -5,7 +5,7 @@ class finite_sp_matrix(finite_matrix):
     def __init__(self, list_representation):
         if isinstance(list_representation, finite_matrix):
             #promote finite matrix to sp finite_matrix
-            assert list_representation.is_symplectic()
+            #assert list_representation.is_symplectic()
             self.elements = list_representation.elements
             self.p=list_representation.p
             self.n=list_representation.n
@@ -17,18 +17,35 @@ class finite_sp_matrix(finite_matrix):
         if isinstance(other, point_of_plane):
             x = other.x
             y = other.y
-            assert len(self.elements[0])==  2 * x.n
+            if len(self.elements[0])==  2: #when self is an element of sl_2
+                col_matrix = finite_matrix([[x],[y]])
+                new_col_matrix = self * col_matrix
+                new_row_matrix = new_col_matrix.transpose()
+                new_x = new_row_matrix.elements[0][0]
+                new_y = new_row_matrix.elements[0][1]
+                return point_of_plane((new_x, new_y))
+            else: #for a prime field matrix.
+                vector = x.to_vector() + y.to_vector()
+                col_matrix = finite_matrix(vector)
+                col_matrix = self * col_matrix #This time, we are multiplying matrices!
+                row_matrix = col_matrix.transpose()
+                new_vector = row_matrix.elements[0]
+                new_x_vector = new_vector[:len(new_vector)//2]
+                new_y_vector = new_vector[len(new_vector)//2:]
+                new_x = finite_field_element.from_vector(new_x_vector)
+                new_y = finite_field_element.from_vector(new_y_vector)
+                return point_of_plane((new_x,new_y))
+        elif isinstance(other, line_of_plane):
+            #TODO: rewrite this in terms of the coefficients of line.
+            iter = other.gen_points()
+            pt1 = next(iter)
+            pt2 = next(iter)
+            new_pt1 = self * pt1
+            new_pt2 = self * pt2
+            return new_pt1.line_to(new_pt2)
 
-            vector = x.to_vector() + y.to_vector()
-            col_matrix = finite_matrix(vector)
-            col_matrix = self * col_matrix #This time, we are multiplying matrices!
-            row_matrix = col_matrix.transpose()
-            new_vector = row_matrix.elements[0]
-            new_x_vector = new_vector[:len(new_vector)//2]
-            new_y_vector = new_vector[len(new_vector)//2:]
-            new_x = finite_field_element.from_vector(new_x_vector)
-            new_y = finite_field_element.from_vector(new_y_vector)
-            return point_of_plane((new_x,new_y))
+
+
         elif isinstance(other, finite_sp_matrix):
             return finite_sp_matrix(super().__mul__(other))
         else: #when other is a finite_matrix.
@@ -40,6 +57,17 @@ class finite_sp_matrix(finite_matrix):
     def inverse(self):
         return finite_sp_matrix(super().inverse())
 
+    def orbit(self, other):
+        #assert isinstance(other, point_of_plane) or isinstance(other, line_of_plane)
+        #Yay polymorphism!
+        to_return = [other]
+        current = self*other
+        count =0
+        while not current == other:
+            to_return.append(current)
+            current = self * current
+        return to_return
+
     def block_matrix_decompose(self):
         #returns a list-of-lists of matrices [[A,B][C,D]]
         size = len(self.elements)
@@ -50,6 +78,19 @@ class finite_sp_matrix(finite_matrix):
         D_elements = [[self.elements[j][i] for i in range(block_size,size)] for j in range(block_size,size)]
         return [[finite_matrix(A_elements), finite_matrix(B_elements)],[finite_matrix(C_elements), finite_matrix(D_elements)]]
 
+    def sl_matrix_to_prime_matrix(self,p,n):
+        #takes an sl_2 matrix and produces the equivalent prime matrix.
+        if n == 1:
+            return self
+        prime_matrix = self.to_prime_field_matrix()
+        convert = finite_matrix.dual_basis_conversion[(p,n)]
+        I = finite_matrix.identity(n,p,1)
+        O = finite_matrix.zero(n,p,1)
+        factor1 = finite_matrix([[convert,O],[O,I]])
+        factor2 = finite_matrix([[convert.inverse(),O],[O,I]])
+        prime_matrix = factor1 * prime_matrix * factor2
+        prime_matrix = finite_sp_matrix(prime_matrix)
+        return prime_matrix
 
     def factorize(self):
         #TODO: Is this method totally general? Is it necessary that either C or D is invertible?
@@ -64,7 +105,7 @@ class finite_sp_matrix(finite_matrix):
         if D.determinant().is_zero():
             J = finite_sp_matrix.B_type_matrix(finite_matrix.identity(len(self.elements)//2,self.p,self.n))
             temp = self*J
-            if B.determinant().is_zero():
+            if C.determinant().is_zero():
                 print("problem")
             fact = temp.factorize()
             return fact[0], fact[1], J.inverse()
@@ -80,15 +121,16 @@ class finite_sp_matrix(finite_matrix):
             assert final_blocks[0][1].is_zero()
             upper = (factor_2*factor_1).inverse()
             #upper is triangular, lower is strictly triangular, and the last matrix is either J inverse or the identity
+            #print(lower)
             return upper, lower, finite_matrix.identity(len(self.elements),self.p,self.n)
 
     def factorize_upper(self):
         #assume self is a block upper triangular matrix
         blocks = self.block_matrix_decompose()
-        assert blocks[1][0].is_zero()
+        #assert blocks[1][0].is_zero()
         M = blocks[0][0]
         A = blocks[0][1]
-        assert not M.determinant().is_zero()
+        #assert not M.determinant().is_zero()
         factor1 = finite_sp_matrix.C_type_matrix(M)
         factor2 = finite_sp_matrix.A_type_matrix(M.inverse()*A)
         assert factor1 * factor2 == self
@@ -96,6 +138,7 @@ class finite_sp_matrix(finite_matrix):
 
     def factorize_lower(self):
         #assume self is a strictly block lower triangular matrix.
+        #print(self)
         blocks = self.block_matrix_decompose()
         assert blocks[0][1].is_zero()
         assert blocks[0][0]==blocks[1][1] #these should both be the identity.
@@ -106,10 +149,11 @@ class finite_sp_matrix(finite_matrix):
         return J, factor, J.inverse()
 
 
+
     def A_type_weil(self):
         blocks = self.block_matrix_decompose()
         A = blocks[0][1]
-        assert self == finite_sp_matrix.A_type_matrix(A)
+        #assert self == finite_sp_matrix.A_type_matrix(A)
         size = len(blocks[0][0].elements)
         diag = []
         two_inv = finite_field_element([2],self.p,1).inverse()
@@ -124,7 +168,7 @@ class finite_sp_matrix(finite_matrix):
 
     def C_type_weil(self):
         blocks = self.block_matrix_decompose()
-        assert blocks[0][1] == blocks[1][0]
+        #assert blocks[0][1] == blocks[1][0]
         C = blocks[0][0]
         C_inv_trans = C.inverse().transpose()
         permutation = [] #the result should be a permutation matrix.
@@ -140,29 +184,35 @@ class finite_sp_matrix(finite_matrix):
             col_matrix = finite_matrix([[y] for y in yps])
             result = C_inv_trans * col_matrix
             permutation.append( col_matrix_to_int(result) )
-        result = np.matrix([[1 if i == permutation[x] else 0 for x in range(self.p**size)] for i in range(self.p**size)])
+        result = np.matrix([[1 if row == permutation[column] else 0 for column in range(self.p**size)] for row in range(self.p**size)])
+
         multiplier = legendre_symbol(int(C.determinant()), self.p)
+        #multiplier = C.determinant().square_class()
+        #print(multiplier)
         return multiplier*result
 
     def B_type_weil(self):
         #only defined for the symplectic form over a prime field
-        assert self == finite_matrix.symplectic_form(len(self.elements),self.p,1)
-        neg_two_inv = -finite_field_element([2],self.p,1).inverse()
+        #assert self == finite_matrix.symplectic_form(len(self.elements),self.p,1)
+        neg_two_inv = -(finite_field_element([2],self.p,1).inverse())
         to_return = []
         guass_sum = 0
         size = len(self.elements)//2
+        B = self.block_matrix_decompose()[0][1]
         #size = len(blocks[0][0].elements)
         for ys in itertools.product(finite_field_element.list_elements(self.p,1), repeat = size):
             current_col = []
             yps = list(ys)
             yps.reverse()
-            row_y = finite_matrix(yps).transpose()
-            guass_sum += (finite_matrix([[neg_two_inv]]) * (row_y * row_y.transpose()) ).character()
+            col_y = finite_matrix([[y] for y in yps])
+            row_y = col_y.transpose()
+            guass_sum += (finite_matrix([[neg_two_inv]]) * (row_y * B * col_y) ).character()
             for xs in itertools.product(finite_field_element.list_elements(self.p,1), repeat = size):
                 xps = list(xs)
                 xps.reverse()
-                col_x = finite_matrix(xps)
-                val = (finite_matrix([[neg_two_inv]]) * (row_y * col_x) ).character()
+                col_x = finite_matrix([[x] for x in xps])
+                #val = (finite_matrix([[neg_two_inv]]) * (row_y * B * col_x) ).character()
+                val = ( (row_y * B * col_x) ).character()
                 current_col.append(val)
             to_return.append(current_col)
         to_return = np.matrix(to_return).T
@@ -173,23 +223,42 @@ class finite_sp_matrix(finite_matrix):
         upper_blocks = upper.block_matrix_decompose()
 
         up_factor1, up_factor2 = upper.factorize_upper()
+        assert up_factor1 * up_factor2 == upper
+        #print(up_factor1, up_factor2)
         low_factor1, low_factor2, low_factor3 = lower.factorize_lower()
+        # print("ss")
+        # print(low_factor1, low_factor2, low_factor3)
         assert up_factor1 * up_factor2 * low_factor1 * low_factor2 * low_factor3 * last == self
-        weil_of_j = finite_sp_matrix(finite_matrix.symplectic_form(len(self.elements),self.p,1)).B_type_weil()
-        weil_of_j_inv = np.linalg.inv(weil_of_j)
-
+        J = finite_sp_matrix(finite_matrix.symplectic_form(len(self.elements),self.p,1))
+        assert low_factor1 == J
+        assert low_factor3 == J.inverse()
+        weil_of_j = J.B_type_weil()
+        #weil_of_j_inv = np.linalg.inv(weil_of_j)
+        weil_of_j_inv = weil_of_j.H
+        #assert np.allclose(np.eye(5,5), weil_of_j @ weil_of_j_inv)
+        # print(weil_of_j @ weil_of_j)
+        # print((J*J).C_type_weil())
+        assert np.allclose((J*J).C_type_weil(),weil_of_j @ weil_of_j)
+        #print("pass")
+        #assert np.allclose(np.eye(5,5),  weil_of_j @ weil_of_j @ weil_of_j @ weil_of_j)
+        #print (J*J)
         assert is_unitary(up_factor1.C_type_weil())
         assert is_unitary(up_factor2.A_type_weil())
         assert is_unitary(weil_of_j)
         assert is_unitary(low_factor2.A_type_weil())
         #assert is_unitary()
-        if last == finite_matrix.symplectic_form(len(self.elements),self.p,1).inverse():
-            to_return = up_factor1.C_type_weil() * up_factor2.A_type_weil() * weil_of_j * low_factor2.A_type_weil() * weil_of_j_inv * weil_of_j_inv
+        if last == J.inverse():
+            #print("occurs")
+            to_return = up_factor1.C_type_weil() @ up_factor2.A_type_weil() @ weil_of_j @ low_factor2.A_type_weil() @ weil_of_j_inv @ weil_of_j_inv
         else:
-            to_return = up_factor1.C_type_weil() * up_factor2.A_type_weil() * weil_of_j * low_factor2.A_type_weil() * weil_of_j_inv
-        assert is_unitary(to_return)
+            #print("echo")
+            to_return = up_factor1.C_type_weil() @ up_factor2.A_type_weil() @ weil_of_j @ low_factor2.A_type_weil() @ weil_of_j_inv
+        #assert is_unitary(to_return)
         return to_return
 
+    @classmethod
+    def identity(cls, size,p,n):
+        return finite_sp_matrix(finite_matrix.identity(size,p,n))
 
     #The following generators of the symplectic group come from https://www.math.wisc.edu/~shamgar/Small-Representations-Howe70th-Proceedings.pdf
     @classmethod
@@ -233,7 +302,7 @@ class finite_sp_matrix(finite_matrix):
     @classmethod
     def list_sp(cls, size, p, n):
         #TODO: finish this method.
-        assert size % 2 == 0
+        #assert size % 2 == 0
         for A0 in finite_matrix.list_symmetric_matrices(size//2,p,n):
             for C in finite_matrix.list_invertible_matrices(size//2,p,n):
                 pass

grid_class.py
@@ -35,7 +35,7 @@ class grid_element(object_modified):
         marginal = []
         for l in line.gen_parallel_lines():
             marginal.append(sum([self.get_value(pt) for pt in l.gen_points()]) )
-        assert(abs (sum(marginal) -1) < epsilon)
+        #assert(abs (sum(marginal) -1) < epsilon)
         return marginal
 
     def marginalize_grid(self, line):
@@ -45,10 +45,21 @@ class grid_element(object_modified):
         return self.marginals[str(cached_line)]
 
     def sum_line(self, line):
-        if line is None:
-            return None
-        #returns the sum of the values over the line
-        return np.sum([self.get_value(pt) for pt in line.gen_points()])
+        if line.isNone():
+            return 0
+        cached_line = line.parallel_through(point_of_plane.origin(self.p,self.n))
+        if not line.coefficients[0].is_zero():
+            x = int(line.coefficients[2]/line.coefficients[0])
+        else:
+            x = int(line.coefficients[2]/line.coefficients[1])
+        #assert self.marginals[str(cached_line)][x] == sum([self.get_value(pt) for pt in line.gen_points()])
+        return self.marginals[str(cached_line)][x]
+
+        # print("hii")
+        # if line is None:
+        #     return None
+        # #returns the sum of the values over the line
+        # return np.sum([self.get_value(pt) for pt in line.gen_points()])
 
     def total_negativity(self):
         p,n =self.p, self.n

special_state_scripts.py
@@ -2,40 +2,68 @@ from finite_sp_matrix_class import *
 from density_matrix_functions import *
 from wigner_function import *
 from grid_class import *
+from covariance_functions import *
 finite_matrix.load_dual_basis_matrices()
-def sl_matrix_to_prime_matrix(sl,p,n):
-    #takes an sl_2 matrix and produces the equivalent prime matrix.
-    prime_matrix = sl.to_prime_field_matrix()
-    convert = finite_matrix.dual_basis_conversion[(p,n)]
-    I = finite_matrix.identity(n,p,1)
-    O = finite_matrix.zero(n,p,1)
-    factor1 = finite_matrix([[convert,O],[O,I]])
-    factor2 = finite_matrix([[convert.inverse(),O],[O,I]])
-    prime_matrix = factor1 * prime_matrix * factor2
-    prime_matrix = finite_sp_matrix(prime_matrix)
-    return prime_matrix
+# def sl_matrix_to_prime_matrix(sl,p,n):
+#     #takes an sl_2 matrix and produces the equivalent prime matrix.
+#     prime_matrix = sl.to_prime_field_matrix()
+#     convert = finite_matrix.dual_basis_conversion[(p,n)]
+#     I = finite_matrix.identity(n,p,1)
+#     O = finite_matrix.zero(n,p,1)
+#     factor1 = finite_matrix([[convert,O],[O,I]])
+#     factor2 = finite_matrix([[convert.inverse(),O],[O,I]])
+#     prime_matrix = factor1 * prime_matrix * factor2
+#     prime_matrix = finite_sp_matrix(prime_matrix)
+#     return prime_matrix
     #unitary = prime_matrix.weil_representation()
 
 
-def test_representation():
-    for s1 in finite_sp_matrix.list_sl_2(3,2):
-        for s2 in finite_sp_matrix.list_sl_2(3,2):
-            s1p =  sl_matrix_to_prime_matrix(s1,3,2)
-            s2p = sl_matrix_to_prime_matrix(s2,3,2)
-
-            unitary1 = s1p.weil_representation()*s2p.weil_representation()
+def test_representation():# This test doesn't do much..
+    p,n = 7,1
+    I = finite_field_element.one(p,n)
+    O = finite_field_element.zero(p,n)
+    #s1 = finite_sp_matrix([[I,O],[I,I]])
+    for s1 in finite_sp_matrix.list_sl_2(p,n):
+        for s2 in finite_sp_matrix.list_sl_2(p,n):
+            s1p = s1
+            s2p = s2
+            unitary1 = s1p.weil_representation() @ s2p.weil_representation()
             unitary2 = (s1p*s2p).weil_representation()
-            assert np.allclose(unitary1,unitary2)
-            #print("accurate")
-        print("s2 exhausted")
+            # print(unitary1)
+            # print(unitary2)
+            print(s1p*s2p)
+            assert np.allclose(s2p.weil_representation() @ s2p.weil_representation(), (s2p*s2p).weil_representation())
+            assert np.allclose(unitary1, unitary2)
+    print("s2 exhausted")
+
+def debugging_test():
+    p,n = 5,1
+    I = finite_field_element.one(p,n)
+    O = finite_field_element.zero(p,n)
+    T = finite_field_element([2],p,n)
+    bad = finite_sp_matrix([[I,I],[I,T]])
+    print(bad.determinant())
+    print( (bad*bad).determinant())
+    #print(bad.weil_representation())
+    assert np.allclose(bad.weil_representation() @ bad.weil_representation(), (bad*bad).weil_representation())
+
+def debug_test2():
+    J = finite_sp_matrix(finite_matrix.symplectic_form(2,5,1))
+    for C in finite_matrix.list_invertible_matrices(1,5,1):
+        C_mat = finite_sp_matrix.C_type_matrix(C)
+        assert np.allclose(J.B_type_weil() @ C_mat.C_type_weil() @ J.B_type_weil(),  (J * C_mat * J).C_type_weil())
+        print("ok")
+    for A in finite_matrix.list_symmetric_matrices(1,5,1):
+        A_mat = finite_sp_matrix.A_type_matrix(A)
+        assert np.allclose( J.B_type_weil() @ A_mat.A_type_weil() @ J.B_type_weil(), (J * A_mat * J).weil_representation() )
+#debug_test2()
+#debugging_test()
+#test_representation()
 
 
-def test_clifford_covariance(sp, density_matrix, p, n):
-    #print(sp)
+#----   TESTS RELATED TO CLIFFORD COVARIANCE ------#
+def test_clifford_covariance_prime_field(sp, density_matrix, p, n):
     unitary = sp.weil_representation()
-    #unitary = sp.C_type_weil()
-    #unitary = sp.A_type_weil()
-    #print(unitary)
     new_density = unitary @ density_matrix @ unitary.H
     grid = grid_element(density_matrix, p, n, multiparticle = True)
     new_grid = grid_element(new_density, p, n, multiparticle = True)
@@ -44,32 +72,57 @@ def test_clifford_covariance(sp, density_matrix, p, n):
         for y in finite_field_element.list_elements(p,n):
             pt = point_of_plane((x,y))
             original_value = grid.get_value(pt)
-            new_pt = sp.inverse() * pt
-            val1 = new_grid.get_value(pt)
-            val2 = grid.get_value(new_pt)
-            # print (original_value)
-            # print (new_value)
-            # print ("--")
+            new_pt = sp * pt
+            val1 = new_grid.get_value(new_pt)
+            val2 = grid.get_value(pt)
+            assert np.allclose(val1,val2)
+def test_clifford_covariance_sl_2(sl, density_matrix,p,n):
+    grid = grid_element(density_matrix, p, n)
+    sp = sl.sl_matrix_to_prime_matrix(p,n)
+    unitary = sp.weil_representation()
+    new_density = unitary @ density_matrix @ unitary.H
+    new_grid = grid_element(new_density, p, n)
+    for x in finite_field_element.list_elements(p,n):
+        for y in finite_field_element.list_elements(p,n):
+            pt = point_of_plane((x,y))
+            original_value = grid.get_value(pt)
+            new_pt = sl * pt
+            val1 = new_grid.get_value(new_pt)
+            val2 = grid.get_value(pt)
             assert np.allclose(val1,val2)
 
-test_representation()
-# for s in finite_sp_matrix.list_sl_2(3,2):
-#     prime_matrix = sl_matrix_to_prime_matrix(s,3,2)
-#     density_matrix = random_pure_state(3,2)
-#     test_clifford_covariance(prime_matrix,density_matrix,3,2)
-#
-# n = 2
-# for A in finite_matrix.list_symmetric_matrices(n,3,1):
-#     prime_matrix= finite_sp_matrix.A_type_matrix(A)
-#     #prime_matrix = sl_matrix_to_prime_matrix(s,3,1)
-#     print(prime_matrix)
-#     density_matrix = random_pure_state(3,n)
-#     print(density_matrix)
-#     test_clifford_covariance(prime_matrix,density_matrix,3,n)
-# for C in finite_matrix.list_invertible_matrices(2,5,1):
-#     prime_matrix= finite_sp_matrix.C_type_matrix(C)
-#     #prime_matrix = sl_matrix_to_prime_matrix(s,3,1)
-#     print(prime_matrix)
-#     density_matrix = random_pure_state(5,2)
-#     #print(density_matrix)
-#     test_clifford_covariance(prime_matrix,density_matrix,5,2)
+def test_clifford_covariance():
+    for s in finite_sp_matrix.list_sl_2(3,2):
+        density_matrix = random_pure_state(3,2)
+        test_clifford_covariance_sl_2(s,density_matrix,3,2)
+        print(s)
+    print("covariance sl2 passed")
+    for s in finite_sp_matrix.list_sl_2(3,2):
+        prime_matrix = sl_matrix_to_prime_matrix(s,3,2)
+        density_matrix = random_pure_state(3,2)
+        test_clifford_covariance_prime_field(prime_matrix,density_matrix,3,2)
+
+def test_generate_state_from_sl():
+    p,n = 3,2
+    for sl in [s for s in finite_sp_matrix.list_sl_2(p,n) if s.order()==p**n+1]:
+        sp = sl.sl_matrix_to_prime_matrix(p,n)
+        U = sp.weil_representation()
+        for i in range(p**n):
+            density_matrix = state_from_sl(sl,i,p,n)
+            assert np.allclose(density_matrix, U @ density_matrix @ U.H)
+            grid = grid_element(density_matrix, p, n)
+            for x in finite_field_element.list_elements(p,n):
+                for y in finite_field_element.list_elements(p,n):
+                    pt = point_of_plane((x,y))
+                    print(grid.get_value(pt))
+                    print(grid.get_value(sl*pt))
+                    assert np.allclose(grid.get_value(pt), grid.get_value(sl*pt))
+test_generate_state_from_sl()
+# p,n = 5,1
+# for sl in [s for s in finite_sp_matrix.list_sl_2(p,n) if s.order()==p**n+1]:
+#     density_matrix = state_from_sl(sl,0,p,n)
+#     test_clifford_covariance_prime_field(sl, density_matrix, p, n)
+test_clifford_covariance()
+
+
+#----